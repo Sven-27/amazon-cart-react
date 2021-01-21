9ea7eb6e8f7205236c47410bd4f58884282f16f7
Sven Notermans <sven.notermans@live.nl>
2021-01-21 16:21:22 +0100

    add: styling

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -13,7 +13,14 @@ function CartItem() {
                 </div>
                 <div className="cartItem__info__stock">In Stock</div>
                 <div className="cartItem__item__actions">
-                    <div className="cartItem__item__quantity">Select</div> |
+                    <div className="cartItem__item__quantity">
+                    <select className="cartItem__select">
+                    <option value="1">Qty: 1</option>
+                    <option value="2">Qty: 2</option>
+                    <option value="3">Qty: 3</option>
+                    </select>    
+                    </div> 
+                    <div className="cartItem__divider">|</div>
                     <div className="cartItem__item__delete">Delete</div>
                 </div>
             </div>
