fb84ea15f3b0261a20ce146ae21bbc15d083ef3f
Sven Notermans <sven.notermans@live.nl>
2021-01-21 19:18:41 +0100

    add carttotal

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -4,7 +4,14 @@ import './CartTotal.css'
 function CartTotal() {
     return (
         <div className="cartTotal">
-            <h1>Cart Total</h1>
+            <h3>Subtotal(5 items):
+                <span className="cartTotal__price">
+                    €2,843.45
+                </span>
+            </h3>
+            <button className="cartTotal__button">
+                Proceed to checkout
+            </button>
         </div>
     )
 }
